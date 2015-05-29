62a40eab25a70a4159a9952ced8da167c0bbe57b
Bruno <bruno@home.home>
2015-05-30 01:28:58 +0200

    messageName

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var fullMessageLabel: UILabel!
+    @IBOutlet weak var sendMailButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailAction(sender: UIButton) {
+        if(nameTextField.text != "") {
+            messageLabel.hidden = false
+            fullMessageLabel.hidden = false
+            var destinataire = nameTextField.text
+            messageLabel.text = "To : " + destinataire
+            nameTextField.text = ""
+            nameTextField.resignFirstResponder()
+            fullMessageLabel.text = messageTextField.text
+            messageTextField.text = "";
+            
+            sendMailButton.setTitle("Message envoyé", forState: UIControlState.Normal)
+        } else {
+            
+        }
+    }
 
 }
 
